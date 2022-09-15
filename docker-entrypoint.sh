f2e0c08b71fa68e87497cfd6f12668a26c3b4064
Andy <andy@savage.hk>
2022-09-16 00:27:40 +0800

    Create wireguard template Updated generate wireguard script Updated docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,15 @@ PRIVADO_USERNAME="${PRIVADO_USERNAME:-""}"
 PRIVADO_PASSWORD="${PRIVADO_PASSWORD:-""}"
 PRIVADO_VPN_SERVERS="${PRIVADO_VPN_SERVERS:-""}"
 WIREGUARD_CONF_STARTING_NUM="${WIREGUARD_CONF_STARTING_NUM:-"0"}"
-WIREGUARD_FILE_OUTPUT_DIR="${WIREGUARD_FILE_OUTPUT_DIR:-"/config/wireguard"}"
+WIREGUARD_FILE_OUTPUT_DIR="${WIREGUARD_FILE_OUTPUT_DIR:-"/config"}"
 ORIG_CONTAINER_INIT="/init"
+LOG_PREFIX="Privado VPN: "
+
+if ! command -v envsubst >/dev/null 2>&1; then
+  export DEBIAN_FRONTEND=noninteractive
+  echo "${LOG_PREFIX}Installing envsubst package ..."
+  apt-get update -qq && apt-get -qq -y install "gettext-base"
+fi
 
 if [ -n "$PRIVADO_VPN_SERVERS" ]; then
   COUNTER=$WIREGUARD_CONF_STARTING_NUM
@@ -15,7 +22,7 @@ if [ -n "$PRIVADO_VPN_SERVERS" ]; then
     COUNTER=$((COUNTER+1))
   done
 else
-  echo >&2 "NOTE: PRIVADO_VPN_SERVERS is empty skipping wireguard config generation"
+  echo >&2 "${LOG_PREFIX}NOTE: PRIVADO_VPN_SERVERS is empty skipping wireguard config generation"
 fi
 
 # Run our origional container init
